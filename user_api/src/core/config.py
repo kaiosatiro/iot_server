45ccf59e000ac72e844c6bb907dc4a0a0457a69b
kaiosatiro <kaiosatiro@gmail.com>
2024-07-24 18:14:50 -0300

    "Test env vars"

--- user_api/src/core/config.py
+++ user_api/src/core/config.py
@@ -21,7 +21,7 @@ class Settings(BaseSettings):
     POSTGRES_PORT: int = 5432
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
-    POSTGRES_DB: str = ""
+    POSTGRES_DB: str = "app"
 
     @computed_field
     @property
